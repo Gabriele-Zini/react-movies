248647b23c5448aa1c9c439fff7b12ae94e972c8
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2024-03-15 16:18:03 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="card bg-light d-flex justify-content-center align-items-center col-12 col-md-8 col-lg-5 mx-auto mt-5" style={{height:'300px'}}>
+      <h4>My react and bootstrap template</h4>
+    </div>
+    </>
+  )
+}
+
+export default App
