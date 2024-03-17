78b7b45df2a1bdd2864e9fba1c95f4bd01e6d186
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2024-03-17 15:28:21 +0100

    add: no image-available

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,17 +2,21 @@ import PropTypes from "prop-types";
 import "../App.css";
 
 function Card({ movie }) {
- 
   return (
     <>
       <div className="col-12 col-sm-5 col-md-3 col-lg-2 ms-3 mx-auto">
         <div
           className=" ms_card mx-auto"
           style={{
-            backgroundImage: `url(https://image.tmdb.org/t/p/w342/${movie.poster_path})`,
-            backgroundSize: "contain",
+            backgroundImage: `url(${
+              movie.poster_path
+                ? `https://image.tmdb.org/t/p/w342/${movie.poster_path}`
+                : "/src/assets/no-image-available.svg"
+            })`,
+            backgroundSize: `${movie.poster_path ? "contain" : "cover"}`,
+            backgroundPosition: `${!movie.poster_path ? "center" : ""}`,
             backgroundRepeat: "no-repeat",
-            maxWidth:'200px',
+            maxWidth: "200px",
           }}
         >
           <div className="ms_credits d-flex flex-column align-items-start justify-content-center p-2">
@@ -24,9 +28,17 @@ function Card({ movie }) {
               <strong>Cast:</strong>
               {movie.credits.map((credit, index) => {
                 if (index < 5) {
-                  return <span className="mx-1" key={index}>{credit.name},</span>;
+                  return (
+                    <span className="mx-1" key={index}>
+                      {credit.name},
+                    </span>
+                  );
                 } else if (index === 5) {
-                  return <span className="mx-1" key={index}>{credit.name}.</span>;
+                  return (
+                    <span className="mx-1" key={index}>
+                      {credit.name}.
+                    </span>
+                  );
                 } else {
                   return null;
                 }
